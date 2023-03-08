81df415be22c883119cd48dc5482767fe1f094dd
daniNKV <nikiforovdaniel@gmail.com>
2023-03-08 01:30:53 -0300

    Header, background and space for the pokemons list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'main-pattern': "url('/src/img/abstract_background.jpg')",
+      },
+      colors: {
+        background: 'rgb(var(--color-background) )'
+      },
+      borderColor: {
+        background: '#e2dedd'
+      }
+    }
   },
   plugins: [],
 }
