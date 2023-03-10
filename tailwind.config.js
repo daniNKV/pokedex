63b4156456c633f300b70e7fcc85856ed216ea13
daniNKV <nikiforovdaniel@gmail.com>
2023-03-09 23:12:25 -0300

    Iterates over template tile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/*.html"],
   theme: {
     extend: {
       backgroundImage: {
         'main-pattern': "url('/src/img/abstract_background.jpg')",
-        'ul-background': "url('/src/img/pokeball.png')"
+        'ul-background': "url('/src/img/pokeball_inverted.png')"
       },
       colors: {
-        background: 'rgb(var(--color-background) )'
+        background: 'rgb(var(--color-background))',
+        tile: 'hex(var(--color-tile))',
+        textColor: 'hex(var(--color-text))'
       },
       borderColor: {
         background: '#e2dedd'
       },
       backgroundSize: {
-        '80%': '80%'
+        '50%': '50%'
+      },
+      fontFamily: {
+        custom: ['"Press Start 2P"']
       }
-
     }
   },
   plugins: [],
