448e5b5e89200db55e66645a4af6d581c0d84126
daniNKV <nikiforovdaniel@gmail.com>
2023-03-20 22:30:39 -0300

    this will be it?

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["/*.{html,js}"],
+  content: ["src/*.{html,js}"],
   theme: {
     screens: {
       'xs': '320px',
