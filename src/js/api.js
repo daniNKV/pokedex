80c5fab24cb5d467120217ee6caa3ab3a2bb6a7a
daniNKV <nikiforovdaniel@gmail.com>
2023-04-01 20:21:17 -0300

    Shrink api functions

--- src/js/api.js
+++ src/js/api.js
@@ -1,29 +1,21 @@
 export async function getPokemons() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-    const pokemons = await response.json();
-
-    return pokemons;
+    return response.json();
 }
 
 export async function getPageOfPokemons(page) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${(page - 1) * 20}&limit=20`);
-    const pokemons = await response.json();
-
-    return pokemons;
+    return await response.json();
 }
 
 export async function getPokemon(ID) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${ID}`);
-    const pokemon = await response.json();
-
-    return pokemon;
+    return await response.json();
 }
 
 export async function getPokemonSpecie(ID) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${ID}`);
-    const pokemon = await response.json();
-
-    return pokemon;
+    return await response.json();
 }
 
 export function getPokemonSprite(ID) {
