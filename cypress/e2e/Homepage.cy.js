0f8545885fb0e9054a3b6f6f4dd87342924b3fa0
daniNKV <nikiforovdaniel@gmail.com>
2023-04-06 15:22:27 -0300

    Add tests for the homepage

--- cypress/e2e/Homepage.cy.js
+++ cypress/e2e/Homepage.cy.js
@@ -1,7 +1,9 @@
-<reference type="Cypress" />
-describe()
-describe('template spec', () => {
-  before(() => {
+// eslint-disable-next-line spaced-comment
+/// <reference types="Cypress" />
+
+describe('Homepage', () => {
+  let fetchPolyfill;
+  beforeEach(() => {
     const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js';
 
     cy.request(polyfillUrl)
@@ -9,11 +11,9 @@ describe('template spec', () => {
         fetchPolyfill = response.body;
       });
 
-    cy.server();
-    cy.route('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20', 'fixture:listado-pagina-1')
-      .as('obtenerPrimeraPagina');
+    cy.intercept('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20', { fixture: 'first-page.json' }).as('getFirstPage');
 
-    cy.visit('http://127.0.0.1:8080', {
+    cy.visit('http://127.0.0.1:5500/src/index.html', {
       onBeforeLoad(contentWindow) {
         // eslint-disable-next-line no-param-reassign
         delete contentWindow.fetch;
@@ -23,4 +23,56 @@ describe('template spec', () => {
       },
     });
   });
+
+  it('Displays the logo', () => {
+    cy.getByData('logo').should('be.visible');
+  });
+
+	describe('Shows pokemons', () => {
+		it('Loads the first 20 pokemons', () => {
+			const POKEMONS_COUNT = 20;
+			cy.getByData('pokemons').children().should('have.length', POKEMONS_COUNT);
+		});
+
+		it('Displays name and image for each pokemon', () => {
+			cy.getByData('pokemon').each(($pokemon) => {
+				cy.wrap($pokemon).children('p').should('be.visible');
+				cy.wrap($pokemon).children('img').should('be.visible');
+			});
+		});
+	});
+
+  describe ('Pagination visibility', () => {
+    beforeEach(() => {
+      cy.getByData('pagination').as('pagination')
+    })
+    it('Pagination should be visible', () => {
+     cy.get('@pagination')
+      .should('be.visible');
+    });
+    it('Should display the number of pages available', () => {
+      const TOTAL_PAGES = 64;
+      cy.get('@pagination').children().get('#total-pages')
+        .should('have.text', TOTAL_PAGES);
+    });
+    it('Should display the navigation form', () => {
+      const CURRENT_PAGE = 1;
+      cy.get('@pagination').children().getByData('navigation-form')
+				.as('navigation-form');
+			cy.get('@navigation-form').children().get('#page-selection')
+				.should('have.value', CURRENT_PAGE);
+			cy.get('@navigation-form').children().get('p').should('contain','Page');
+			cy.get('@navigation-form').children().get('p').should('contain','of');
+			cy.get('@navigation-form').children().get('#seek-button').should('have.text','GO!');
+    });
+    it('Should not display the previous button', () => {
+      cy.get('@pagination').children().get('#previous-button')
+				.should('not.be.visible');
+    });
+    it('Should display the next button', () => {
+      cy.get('@pagination').children().get('#next-button')
+				.should('be.visible');
+    });
+  });
+
 })
