acd021b53eae8e7121d20272ace215868459988f
daniNKV <nikiforovdaniel@gmail.com>
2023-06-22 00:20:35 -0300

    Use mappers and lint fixes

--- src/js/services/pokemon.js
+++ src/js/services/pokemon.js
@@ -1,45 +1,50 @@
 import {
-    getPokemons as getPokemonsFromApi,
-    getPokemon as getPokemonFromApi,
-    getPokemonSpecie as getPokemonSpecieFromApi,
+	getPokemons as getPokemonsFromApi,
+	getPokemon as getPokemonFromApi,
+	getPokemonSpecie as getPokemonSpecieFromApi,
+	getPokemonSprite,
 
 } from './api/pokemon.js';
 
 import {
-    getPokemons as getPokemonsFromStorage,
-    getPokemon as getPokemonFromStorage,
-    savePokemons,
-    savePokemon,
+	getPokemons as getPokemonsFromStorage,
+	getPokemon as getPokemonFromStorage,
+	savePokemons,
+	savePokemon,
 } from './storage/pokemon.js';
+import { mapPokemon, mapPokemonList } from './mappers/pokemon';
 
 const POKEMONS_LIMIT = 20;
 
 export async function getPokemons(page, limit = POKEMONS_LIMIT) {
-    try {
-        return getPokemonsFromStorage(page);
-    } catch (e) {
-        const pokemons = await getPokemonsFromApi(page);
-        savePokemons(page, limit, pokemons);
+	let pokemons;
 
-        return pokemons;
-    }
+	try {
+		return getPokemonsFromStorage(page);
+	} catch (e) {
+		const pokemonsApi = await getPokemonsFromApi(page);
+		pokemons = mapPokemonList(pokemonsApi, getPokemonSprite);
+		savePokemons(page, limit, pokemons);
+
+		return pokemons;
+	}
 }
 
 export async function getPokemon(id) {
-    if (id === undefined) {
-        throw new Error('No id provided');
-    }
+	if (id === undefined) {
+		throw new Error('No id provided');
+	}
 
-    let pokemon;
+	let pokemon;
 
-    try {
-        pokemon = getPokemonFromStorage(id);
-    } catch (e) {
-        const main = await getPokemonFromApi(id);
-        const specie = await getPokemonSpecieFromApi(id);
-        pokemon = { main, specie };
-        savePokemon(id, pokemon);
-    }
+	try {
+		pokemon = getPokemonFromStorage(id);
+	} catch (e) {
+		const main = await getPokemonFromApi(id);
+		const specie = await getPokemonSpecieFromApi(id);
+		pokemon = mapPokemon(main, specie);
+		savePokemon(id, pokemon);
+	}
 
-    return pokemon;
+	return pokemon;
 }
